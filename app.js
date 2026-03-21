80f1e8c1c9befecd21f0b03677ed401c659169d1
Ruby Kumari R <rubykumarir772@gmail.com>
2026-03-21 11:05:44 +0530

    Added index and show routes with EJS views (Read functionality)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
+const path=require("path");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
@@ -13,22 +14,39 @@ async function main(){
    await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+
 app.get("/",(req,res)=>{
     res.send("Hi,I am root");
 })
-app.get("/testListing",async (req,res)=>{
-    let sampleListing=new Listing({
-        title:"My new Villa",
-        description:"By the beach",
-        price:1200,
-        location:"Calangute,Goa",
-        country:"India"
-    });
-   await sampleListing.save();
-   console.log("sample was saved");
-   res.send("successful testing");
 
+//INDEX ROUTE-LIST ALL THE PLACES PLACED IN THE DATABASE
+app.get("/listings",async (req,res)=>{
+    const allListings=await Listing.find({});
+    res.render("listings/index", { allListings });
 });
+
+//SHOW ROUTE-INDIVIDUAL LISTING KA PURA KA PURA DATA 
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing=new Listing({
+//         title:"My new Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Calangute,Goa",
+//         country:"India"
+//     });
+//    await sampleListing.save();
+//    console.log("sample was saved");
+//    res.send("successful testing");
+
+// });
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
