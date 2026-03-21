00c3ac4f90ce98f08f6e168350633d3e017aaec5
Ruby Kumari R <rubykumarir772@gmail.com>
2026-03-21 09:41:13 +0530

    Initial backend setup: added Mongoose model and seeded database

--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,32 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const listingSchema=new Schema({
-    title:{
-        type:String,
-        required:true,
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
     },
-    description:String,
-    image:{
-        type:String,
-        set:(v)=>v===""?"https://th.bing.com/th/id/OIP.UWZ4uz53WdWyPxDISmzARgHaE7?w=225&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3":v,
+    url: {
+      type: String,
+      default: "https://th.bing.com/th/id/OIP.UWZ4uz53WdWyPxDISmzARgHaE7?w=225&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
+      set: (v) =>
+        v === ""
+          ? "https://th.bing.com/th/id/OIP.UWZ4uz53WdWyPxDISmzARgHaE7?w=225&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
+          : v,
     },
-    price:Number,
-    location:String,
-    country:String,
+  },
+
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing=mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
